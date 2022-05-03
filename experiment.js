1df46c225c5409c19d6e1e2620b57016d2a3b3d3
John Lopez <johnllopez616@gmail.com>
2022-05-03 16:04:09 -0700

    More trial

--- experiment.js
+++ experiment.js
@@ -300,15 +300,15 @@ var answers = [2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
 var held_ball = 0
 
 function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == variable) {
-            return decodeURIComponent(pair[1]);
-        }
-    }
-    console.log('Query variable %s not found', variable);
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+  console.log('Query variable %s not found', variable);
 }
 //Taken from https://stackoverflow.com/questions/2090551/parse-query-string-in-javascript?
 
